4abe8536663efa968a357231d73e4b9eee473a30
katooomi <manami.katooo@gmail.com>
2021-08-11 01:01:12 +0900

    修正（１）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email,  presence: true
-  validates :encrypted_password,  presence: true
-  validates :nickname,  presence: true
-  validates :last_name,  presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth, presence: true
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+ with_options presence: true do
+  validates :email
+  validates :encrypted_password 
+  validates :nickname 
+  validates :last_name 
+  validates :first_name
+  validates :last_name_kana
+  validates :first_name_kana
+  validates :birth
+  
+   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
-  end
+   end
+  
+   with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' } do
+      validates :first_name_kana
+      validates :last_name_kana
+   end
+ end
 end

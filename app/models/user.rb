bb3bc48594c92e4b30f68c4c2f9bfb52432e3a0f
katooomi <manami.katooo@gmail.com>
2021-08-11 12:16:19 +0900

    修正（２）

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,17 @@ class User < ApplicationRecord
 
  with_options presence: true do
   validates :email
-  validates :encrypted_password 
+  validates :password 
   validates :nickname 
   validates :last_name 
   validates :first_name
   validates :last_name_kana
   validates :first_name_kana
   validates :birth
+
+  with_options format: { with: (/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i), message: '半角英数字混同にしてください' } do
+    validates :password
+   end
   
    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name

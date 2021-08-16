417e9983328495d4ef6a3f514144542cdb007eb4
katooomi <manami.katooo@gmail.com>
2021-08-16 15:37:36 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
-  before_action :set_item, only: [:show,:edit,:update]
+  before_action :authenticate_user!, except: [:index,:show,]
+  before_action :set_item, only: [:show,:edit,:update,:destroy]
   before_action :move_to_index, except:[:show,:index,:new,:create]
   
   def index
@@ -33,6 +33,14 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    end
+  end
+
+
 
 
   private

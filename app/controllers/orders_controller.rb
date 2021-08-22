9442c562ad648e640cf6be1464431e8053f8ec4b
katooomi <manami.katooo@gmail.com>
2021-08-22 18:13:21 +0900

    商品購入機能

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,39 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+
+  def index
+    @order = Order.new
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+  private
+ 
+  def order_params
+    params.require(:order_address).permit(:zip_code, :prefecture_id, :city, :street, :building, :phone).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
+  end
+  
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,  
+        card: order_params[:token],    
+        currency: 'jpy'                
+      )
+  end
+
+end
---
+++ app/models/order_address.rb
@@ -0,0 +1,28 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :zip_code, :prefecture_id, :city, :street,:building, :phone, :user_id, :item_id, :token
+  validates :token, presence: true
+
+
+  validates :zip_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  
+  with_options presence: true do
+    validates :city
+    validates :street
+    validates :user_id
+    validates :item_id
+  end  
+  
+  validates :phone, presence: true, format: {with: /\A\d{10,11}\z/, message: "is invalid. 10 or 11 number"}
+   
+
+
+  def save
+  # binding.pry
+   order = Order.create(user_id: user_id, item_id: item_id)
+   Address.create(zip_code: zip_code, prefecture_id: prefecture_id, city: city, street: street, building: building, phone: phone, order_id: order.id)
+  end
+
+
+end
